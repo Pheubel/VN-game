b8202d5c022f4ee234fb583cc014982823f5532c
Pheubel <luca.guagliardo@live.nl>
2018-11-03 17:06:43 +0100

    set up the unity project

---
+++ Visual Novel game/Assets/Scripts/SpriteMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour {
+
+    [SerializeField] private float _speedX;
+    [SerializeField] private float _speedY;
+
+    // Update is called once per frame
+    void Update () {
+        transform.position += new Vector3(_speedX,_speedY);
+	}
+}
